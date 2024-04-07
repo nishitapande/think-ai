813c6f3083af5770d47135eaf016a5907e9c486d
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-04-07 20:26:40 +0530

    .

--- server/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const eventController = require("../controllers/eventController");
-
-router.get("/", eventController.getAllEvents);
-router.post("/createevent", eventController.createEvent);
-
-module.exports = router;
--- server/routes/teamMemberRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const teamController = require("../controllers/teamController");
-
-router.get("/", teamController.getAllMembers);
-router.post("/newmember", teamController.createMember);
-
-module.exports = router;
